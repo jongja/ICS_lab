bfa8f49f7e779cdc1ada81ac8a0acb7ce19c7152
Jongja <whwhdgus94@naver.com>
2019-01-22 16:45:25 +0900

    make study folder

---
+++ study/ML/week1/week_1.py
@@ -0,0 +1,29 @@
+#Linear Regression
+
+
+# X and Y data
+x_train = [1, 2, 3, 4, 5, 6]
+y_train = [1, 2, 3, 4, 5, 6]
+
+y_data = x_data * W + b
+W = tf.Variable(tf.random_normal([1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+hypothesis = x_train * W + b
+
+cost = tf.reduce_mean(tf.square(hypothesis - y_train))
+
+# Minimize
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+train = optimizer.minimize(cost)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+# Fit the line
+for step in range(2001):
+    sess.run(train)
+    if step % 20 == 0:
+        print(step, sess.run(cost), sess.run(W), sess.run(b))
+
+
