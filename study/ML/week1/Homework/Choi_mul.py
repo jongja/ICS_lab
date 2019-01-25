b47838f82472850d50f2b8711fbbd7ec252b5e28
RndelQndel <cyl941028@gmail.com>
2019-01-25 16:03:17 +0900

    yongrak

--- study/ML/week1/Homework/Choi_mul.py
+++ study/ML/week1/Homework/Choi_mul.py
@@ -1,3 +1,4 @@
-for i in range(1,20):
-    for j in range(1,20):
-        print("{} x {} = {}".format(i,j,i*j))
+for i in range(1,20,+1):
+    for j in range(1,20,+1):
+        print("%3dx%3d=%3d|"%(j,i,i*j),end="")
+    print("")
