0274ea0bb275fba332e633cc975aa6c2f094ffff
taehyun9203 <41182842+taehyun9203@users.noreply.github.com>
2019-01-28 22:17:51 +0900

    Update example1_taehyun.py

--- study/ML/week1/Homework/example1_taehyun.py
+++ study/ML/week1/Homework/example1_taehyun.py
@@ -1,28 +1,15 @@
-#for x in range(1, 7, 2):
-    #print((" " * ((7 * 2 - 1 - x) // 2)) + ("i" * x))
-#for y in range(7, 0, -2):
-#    print((" " * ((7 * 2 - 1 - y) // 2)) + "i" * y)
-
-for x in range(0, 6, 2):
-    if x == 0:
-        print(" " * (6-x // 2), end = "")
-        print("0")
-    if x == 2:
-        print(" " * (6 - x // 2), end = "")
-        print("010")
-    if x == 4:
-        print(" " * (6 - x // 2), end = "")
-        print("01210")
-for y in range(6,-2,-2):
-    if y == 6:
-        print(" " * (6 - y // 2), end = "")
-        print("0123210")
-    if y == 4:
-        print(" " * (6 - y // 2), end = "")
-        print("01210")
-    if y == 2:
-        print(" " * (6 - y // 2), end = "")
-        print("010")
-    if y == 0:
-        print(" " * (6 - y // 2), end = "")
-        print("0")
+k = int(input("k :: "))
+for x in range(1, k, 2):
+    print(" "* ((k - x) // 2),end="")
+    for z in range(0, (x-1)//2 , 1):
+        print(z,end="")
+    for j in range((x-1)//2, -1, -1):
+        print(j,end="")
+    print()
+for y in range(k, 0, -2): #
+    print(" " * ((k - y) // 2),end="")
+    for i in range(0, (y-1)//2, 1):
+        print(i,end="")
+    for l in range((y-1)//2, -1, -1):
+        print(l,end="")
+    print()
